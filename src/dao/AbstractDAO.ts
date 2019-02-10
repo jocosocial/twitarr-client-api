a043a3ce4eec9e66d699eb5c8fa399d1494b3f57
Benjamin Reed <ranger@opennms.org>
2019-02-10 16:57:15 -0500

    refactor(dao): add an abstract DAO for common code

---
+++ src/dao/AbstractDAO.ts
@@ -0,0 +1,35 @@
+import { IHasHTTP } from '../api/IHasHTTP';
+import { ITwitarrHTTP } from '../api/ITwitarrHTTP';
+
+export abstract class AbstractDAO {
+  /**
+   * The [[ITwitarrHTTP]] implementation to use internally when making requests.
+   * @hidden
+   */
+  private httpImpl: ITwitarrHTTP;
+
+  /**
+   * Construct a DAO instance.
+   *
+   * @param impl - The HTTP implementation to use.  It is also legal to pass any object
+   *               conforming to the [[IHasHTTP]] interface (like a [[Client]]).
+   */
+  constructor(impl: ITwitarrHTTP | IHasHTTP) {
+    if ((impl as IHasHTTP).http) {
+      impl = (impl as IHasHTTP).http;
+    }
+    this.httpImpl = impl as ITwitarrHTTP;
+  }
+
+  /**
+   * The HTTP implementation to use internally when making DAO requests.
+   */
+  public get http() {
+    return this.httpImpl;
+  }
+
+  public set http(impl: ITwitarrHTTP) {
+    this.httpImpl = impl;
+  }
+
+}
--- src/dao/UserDAO.ts
+++ src/dao/UserDAO.ts
@@ -1,44 +1,12 @@
-import { IHasHTTP } from '../api/IHasHTTP';
-import { ITwitarrHTTP } from '../api/ITwitarrHTTP';
 import { TwitarrHTTPOptions } from '../api/TwitarrHTTPOptions';
 import { TwitarrResult } from '../api/TwitarrResult';
 import { TwitarrError } from '../api/TwitarrError';
 
 import { User } from '../model/User';
 
-import { Util } from '../internal/Util';
+import { AbstractDAO } from './AbstractDAO';
 
-export class UserDAO {
-  /**
-   * The [[ITwitarrHTTP]] implementation to use internally when making requests.
-   * @hidden
-   */
-  private httpImpl: ITwitarrHTTP;
-
-  /**
-   * Construct a DAO instance.
-   *
-   * @param impl - The HTTP implementation to use.  It is also legal to pass any object
-   *               conforming to the [[IHasHTTP]] interface (like a [[Client]]).
-   */
-  constructor(impl: ITwitarrHTTP | IHasHTTP) {
-    if ((impl as IHasHTTP).http) {
-      impl = (impl as IHasHTTP).http;
-    }
-    this.httpImpl = impl as ITwitarrHTTP;
-  }
-
-  /**
-   * The HTTP implementation to use internally when making DAO requests.
-   */
-  public get http() {
-    return this.httpImpl;
-  }
-
-  public set http(impl: ITwitarrHTTP) {
-    this.httpImpl = impl;
-  }
-
+export class UserDAO extends AbstractDAO {
   public async login() {
     const options = new TwitarrHTTPOptions();
     options.data = {
