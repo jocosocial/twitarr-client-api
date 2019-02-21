fa55e7bdd70bd0ba7a648ab4c8f13412bd485e8f
Benjamin Reed <ranger@opennms.org>
2019-02-21 08:52:12 -0500

    feat(dao): profile photo post + delete

--- src/dao/PhotoDAO.ts
+++ src/dao/PhotoDAO.ts
@@ -68,4 +68,41 @@ export class PhotoDAO extends AbstractDAO {
         return PhotoDetails.fromRest(data.photo);
       });
   }
+
+  /**
+   * Post a profile photo.
+   */
+  public async profile(username: string, fileName: string, photoData: Buffer) {
+    const options = new TwitarrHTTPOptions().withHeader('cache-control', 'no-cache');
+
+    let mimeType;
+    if (fileName.endsWith('.png')) {
+      mimeType = 'image/png';
+    } else if (fileName.endsWith('.gif')) {
+      mimeType = 'image/gif';
+    } else if (fileName.endsWith('.jpeg') || fileName.endsWith('.jpg')) {
+      mimeType = 'image/jpeg';
+    } else {
+      throw new TwitarrError('Unable to determine mime-type from filename: ' + fileName);
+    }
+
+    return this.http
+      .postFile('/api/v2/user/photo', fileName, mimeType, photoData, options)
+      .then(result => this.handleErrors(result))
+      .then(data => {
+        return PhotoDetails.fromRest(data.photo);
+      });
+  }
+
+  /**
+   * Remove the user's profile photo.
+   */
+  public async removeProfile() {
+    return this.http
+      .httpDelete('/api/v2/user/photo')
+      .then(result => this.handleErrors(result))
+      .then(data => {
+        return data.md5_hash;
+      });
+  }
 }
